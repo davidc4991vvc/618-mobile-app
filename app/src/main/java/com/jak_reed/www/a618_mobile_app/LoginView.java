8e50a4afbc05ba83b4e1c5e31fe1e5a9ac999191
Jake Reed <jacob.reed95@siu.edu>
2017-05-14 21:40:21 -0500

    Initial commit

---
+++ app/src/main/java/com/jak_reed/www/a618_mobile_app/LoginView.java
@@ -0,0 +1,17 @@
+package com.jak_reed.www.a618_mobile_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class LoginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login_view);
+    }
+}
