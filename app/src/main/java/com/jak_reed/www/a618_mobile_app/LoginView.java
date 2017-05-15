9929dc39f86ab05529c3e51d387bb30df0b81dcd
Jake Reed <jacob.reed95@siu.edu>
2017-05-14 23:43:14 -0500

    New video background

--- app/src/main/java/com/jak_reed/www/a618_mobile_app/LoginView.java
+++ app/src/main/java/com/jak_reed/www/a618_mobile_app/LoginView.java
@@ -1,17 +1,38 @@
 package com.jak_reed.www.a618_mobile_app;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
+import java.net.URI;
+
 public class LoginView extends AppCompatActivity {
+
+    public VideoView backgroundVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login_view);
+
+        backgroundVideo = (VideoView) findViewById(R.id.vapeVideoView);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.vapevidback);
+
+        backgroundVideo.setVideoURI(uri);
+        backgroundVideo.start();
+
+        backgroundVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setVolume(0, 0);
+            }
+        });
     }
 }
