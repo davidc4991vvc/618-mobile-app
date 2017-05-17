487d865836b5a0ff75f498426bc70a2ad15d9373
Jake Reed <jacob.reed95@siu.edu>
2017-05-17 13:32:43 -0500

    creating new elements to register with email

--- app/src/main/java/com/jak_reed/www/a618_mobile_app/LoginWithEmail.java
+++ app/src/main/java/com/jak_reed/www/a618_mobile_app/LoginWithEmail.java
@@ -5,11 +5,16 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.VideoView;
 
 public class LoginWithEmail extends AppCompatActivity {
 
     public VideoView backgroundVideo;
+    public Button registerButton, loginButton;
+    public EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,10 @@ public class LoginWithEmail extends AppCompatActivity {
         setContentView(R.layout.activity_login_with_email);
 
         backgroundVideo = (VideoView) findViewById(R.id.backgroundVideo);
+        registerButton = (Button) findViewById(R.id.register_button);
+        loginButton = (Button) findViewById(R.id.login_button);
+        email = (EditText) findViewById(R.id.email_edit_text);
+        password = (EditText) findViewById(R.id.password_edit_text);
 
         Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.vapevidback);
 
@@ -28,6 +37,20 @@ public class LoginWithEmail extends AppCompatActivity {
             public void onPrepared(MediaPlayer mp) {
                 mp.setVolume(0, 0);
                 mp.setLooping(true);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginWithEmail.this, RegisterAccount.class));
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
--- app/src/main/java/com/jak_reed/www/a618_mobile_app/RegisterAccount.java
+++ app/src/main/java/com/jak_reed/www/a618_mobile_app/RegisterAccount.java
@@ -1,13 +1,39 @@
 package com.jak_reed.www.a618_mobile_app;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class RegisterAccount extends AppCompatActivity {
+
+    public VideoView backgroundVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+
+        backgroundVideo = (VideoView) findViewById(R.id.background_video);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.vapevidback);
+
+        backgroundVideo.setVideoURI(uri);
+        backgroundVideo.start();
+
+        backgroundVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setVolume(0, 0);
+                mp.setLooping(true);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, LoginWithEmail.class));
     }
 }
